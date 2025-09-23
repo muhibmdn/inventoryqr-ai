5d298592792c70220999a1ae7c2f072fd568c3be
Muhib Mdn <muhib.ulum2003@gmail.com>
2025-09-23 13:08:41 +0700

    Change DIRECT_URL to DATABASE_URL

--- src/db.ts
+++ src/db.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-const datasourceUrl = process.env.DIRECT_URL ?? process.env.DATABASE_URL;
+const datasourceUrl = process.env.DATABASE_URL;
 
 if (!datasourceUrl) {
-  throw new Error("DATABASE_URL or DIRECT_URL must be set");
+  throw new Error("DATABASE_URL");
 }
 
 export const db =
